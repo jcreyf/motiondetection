bae76bc3e5be2ccaa16d49d2b29bf718027d7585
Jo Creyf <jo.creyf@gmail.com>
2023-05-05 19:51:57 -0700

    Adding more meat

--- motiondetection.py
+++ motiondetection.py
@@ -18,6 +18,7 @@
 #   - try to zoom into those areas and take snapshots;
 #   - start recording video when movement is detected and stop a few seconds after last movement;
 #   - send a message with the base image and zoomed in pictures.  Also add link to where video will be stored;
+#   - pub/sub actions (have an abstract class that implements the basics (logging to console))
 
 # pip install -r requirements.txt
 import cv2
@@ -41,6 +42,7 @@ class MotionDetector:
         self._camera_number = 0
         self._camera = None
         self._opencv_diffing_threshold = 20
+        self._minimum_pixel_difference = 50
 
 
     def __del__(self) -> None:
@@ -68,6 +70,19 @@ class MotionDetector:
     @diffingThreshold.setter
     def diffingThreshold(self, value: int) -> None:
         self._opencv_diffing_threshold = value
+
+
+    @property
+    def minimumPixelDifference(self) -> int:
+        return self._minimum_pixel_difference
+
+    @minimumPixelDifference.setter
+    def minimumPixelDifference(self, value: int) -> None:
+        self._minimum_pixel_difference = value
+
+
+    def logSettings(self) -> None:
+        pass
 
 
     def listCameras(self):
@@ -131,7 +146,7 @@ class MotionDetector:
             # Dilute the image a bit to make differences more seeable; more suitable for contour detection
             kernel = np.ones((5, 5))
             diff_frame = cv2.dilate(diff_frame, kernel, 1)
-            # Only take different areas that are different enough (>20 / 255)
+            # Only take different areas that are different enough:
             thresh_frame = cv2.threshold(
                 src=diff_frame, thresh=self._opencv_diffing_threshold, maxval=255, type=cv2.THRESH_BINARY
             )[1]
@@ -140,22 +155,26 @@ class MotionDetector:
             contours, _ = cv2.findContours(
                 image=thresh_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE
             )
-            # Comment below to stop drawing contours
-            cv2.drawContours(
-                image=img_rgb,
-                contours=contours,
-                contourIdx=-1,
-                color=(0, 255, 0),
-                thickness=2,
-                lineType=cv2.LINE_AA,
-            )
-            # Uncomment 6 lines below to stop drawing rectangles
-            for contour in contours:
-               if cv2.contourArea(contour) < 50:
-                 # too small: skip!
-                   continue
-               (x, y, w, h) = cv2.boundingRect(contour)
-               cv2.rectangle(img=img_rgb, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
+            # No need to process the image if OpenCV didn't find differences:
+            if len(contours) > 0:
+#                self.logDebug(f"found {len(contours)} differences")
+                # Draw contours around the changed areas:
+                # cv2.drawContours(
+                #     image=img_rgb,
+                #     contours=contours,
+                #     contourIdx=-1,
+                #     color=(0, 255, 0),
+                #     thickness=2,
+                #     lineType=cv2.LINE_AA,
+                # )
+                # Draw rectangles around the changed areas:
+                for contour in contours:
+                    if cv2.contourArea(contour) < self._minimum_pixel_difference:
+                        # The dectected difference area is too small.  Lets ignore it.
+#                        self.logDebug(f"too small {cv2.contourArea(contour)}")
+                        continue
+                    (x, y, w, h) = cv2.boundingRect(contour)
+                    cv2.rectangle(img=img_rgb, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
 
             # Show the processed picture in a window:
             cv2.imshow("Motion detector", img_rgb)
@@ -210,6 +229,7 @@ if __name__ == "__main__":
     motion_detector = MotionDetector()
     motion_detector.debug = __ARGS.debug
     motion_detector.diffingThreshold = 100
+    motion_detector.minimumPixelDifference = 150
     print("Press the 'q' key to stop the app (the camera window needs to have the focus!)")
     motion_detector.doIt()
     print("Ending the app")
