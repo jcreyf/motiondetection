251aea8e1d3a31183f45e987c78189104fc2c650
Jo Creyf <jo.creyf@gmail.com>
2023-05-14 18:38:59 -0700

    Look into adding detection of camera resolutions

--- cam.py
+++ cam.py
@@ -2,18 +2,21 @@
 
 # From:
 #   https://docs.opencv.org/3.4/dd/d43/tutorial_py_video_display.html
+# Resolutions:
+#   https://www.learnpythonwithrune.org/find-all-possible-webcam-resolutions-with-opencv-in-python/
 
 # pip install -r requirements.txt
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
+cam = cv.VideoCapture(0)
+if not cam.isOpened():
     print("Cannot open camera")
     exit()
+
 while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = cam.read()
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
@@ -28,5 +31,5 @@ while True:
         break
 
 # When everything done, release the capture
-cap.release()
+cam.release()
 cv.destroyAllWindows()
